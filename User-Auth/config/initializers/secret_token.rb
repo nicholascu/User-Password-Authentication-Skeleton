6ea96b66b134f0331ee7139b9433682e51ae6664
nicholascu <nicholascu@gmail.com>
2014-06-29 01:28:11 -0700

    Create new rails app User-Auth

---
+++ User-Auth/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserAuth::Application.config.secret_token = '5528dd904b942dd909bed0064426c4f34e5319d0d0918259d8cca661b1aa446a01ad51927f5403a01c5b4adc3f5b2854f759608e6a52b848119da2e73b6a0495'
