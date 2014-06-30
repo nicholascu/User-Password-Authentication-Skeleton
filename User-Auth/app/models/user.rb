74aed545acc24713785cfb3b9d661cd4b19b0b76
nicholascu <nicholascu@gmail.com>
2014-06-29 23:01:34 -0700

    Add Mass Assignment Protection

--- User-Auth/app/models/user.rb
+++ User-Auth/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessor :password
+  attr_accessible :username, :email, :password, :password_confirmation
+  
   EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
   validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
   validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
@@ -16,7 +18,7 @@ class User < ActiveRecord::Base
       self.encrypted_password= BCrypt::Engine.hash_secret(password, salt)
     end
   end
-  
+
   def clear_password
     self.password = nil
   end
