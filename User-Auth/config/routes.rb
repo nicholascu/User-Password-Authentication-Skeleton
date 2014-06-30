056f448ddc5dee94da8cfd3ba33321e324be31a8
nicholascu <nicholascu@gmail.com>
2014-06-29 23:28:18 -0700

    Create Sessions Controller/View

--- User-Auth/config/routes.rb
+++ User-Auth/config/routes.rb
@@ -1,4 +1,12 @@
 UserAuth::Application.routes.draw do
+  get "sessions/login,"
+
+  get "sessions/home,"
+
+  get "sessions/profile,"
+
+  get "sessions/setting"
+
   get "users/new"
 
 resources :users
