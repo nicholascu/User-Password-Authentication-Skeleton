c1ac54204ab6cbd0af0bf7a246510d3476491214
nicholascu <nicholascu@gmail.com>
2014-06-29 23:40:17 -0700

    Remova extra comma in file name

--- User-Auth/config/routes.rb
+++ User-Auth/config/routes.rb
@@ -1,9 +1,9 @@
 UserAuth::Application.routes.draw do
   get "sessions/login,"
 
-  get "sessions/home,"
+  get "sessions/home"
 
-  get "sessions/profile,"
+  get "sessions/profile"
 
   get "sessions/setting"
 
